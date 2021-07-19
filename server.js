517827161a8e034a4dd578b6ea57193e99cf32a6
Iryna <iryna.shulim@gmail.com>
2021-07-19 07:54:01 -0500

    get single candidate

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ const db = mysql.createConnection(
     console.log(rows);
   });
 
+  // GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(row);
+  });
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
